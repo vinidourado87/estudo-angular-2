419e9cc36a7cf4789c039eb3608d2051ff10e549
Vinicius Dourado <vinidourado87@gmail.com>
2016-12-10 21:10:08 -0200

    First commit

---
+++ client/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app',
+    templateUrl: './app.component.html'
+})
+
+export class AppComponent{
+
+    fotos: Object[] = [];
+
+    constructor(http: Http) {
+        let stream = http.get('v1/fotos');
+        /*stream.subscribe(function(res){
+        });*/
+        stream.subscribe(res => {
+            this.fotos = res.json();
+            console.log(this.fotos);
+        });
+    }
+}
