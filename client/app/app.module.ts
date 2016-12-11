6cff55669d8e24643b8224d7c40231c06625f426
Vinicius Dourado <vinidourado87@gmail.com>
2016-12-10 23:07:13 -0200

    Composicao de components

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { FotoModule }  from './foto/foto.module';
+import { PainelModule }  from './painel/painel.module';
 import { HttpModule } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @NgModule({
- imports:[ BrowserModule, FotoModule, HttpModule ],
+ imports:[ BrowserModule, FotoModule, HttpModule, PainelModule ],
  declarations: [ AppComponent ],
  bootstrap: [ AppComponent ]
 })
