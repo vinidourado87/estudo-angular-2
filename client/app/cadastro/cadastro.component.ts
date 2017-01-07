5fa6c610167876af929e660b6889c2194f055d1e
Vinicius Dourado <vinidourado87@gmail.com>
2017-01-07 21:38:56 -0200

    Validacao de campos do lado do backend

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { FotoComponent } from '../foto/foto.component';
 import { Http, Headers } from '@angular/http';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     moduleId: module.id,
@@ -11,9 +12,15 @@ export class CadastroComponent {
 
     foto: FotoComponent = new FotoComponent();
     http: Http;
+    meuForm: FormGroup;
 
-    constructor(http: Http) {
+    constructor(http: Http, formBuilder: FormBuilder) {
         this.http = http;
+        this.meuForm = formBuilder.group({
+            titulo: [ '', Validators.required ],
+            url: [ '', Validators.required ],
+            descricao: [ '' ]
+        });
     }
 
     cadastrar(event) {
