73c3df3d71aada5ef8da40bd973fe5670926aa0a
Vinicius Dourado <vinidourado87@gmail.com>
2017-01-07 09:41:40 -0200

    Salva cadastro no server

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FotoComponent } from '../foto/foto.component';
+import { Http, Headers } from '@angular/http';
 
 @Component({
     moduleId: module.id,
@@ -9,15 +10,21 @@ import { FotoComponent } from '../foto/foto.component';
 export class CadastroComponent {
 
     foto: FotoComponent = new FotoComponent();
+    http: Http;
 
-    constructor() {
-        this.foto.titulo = "Titulo";
-        this.foto.url = "www.teste.com.br";
-        this.foto.descricao = "Desc";
+    constructor(http: Http) {
+        this.http = http;
     }
 
     cadastrar(event) {
         event.preventDefault();
-        console.log(this.foto);
+
+        let head = new Headers();
+        head.append('Content-type', 'application/json');
+
+        this.http.post('v1/fotos', JSON.stringify(this.foto), {headers: head})
+        .subscribe(() => {
+            this.foto = new FotoComponent();
+        }, erro => console.log(erro));
     }
 }
